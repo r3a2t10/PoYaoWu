0e160f8e88ff669a6b15330c2838cef1e9140a6e
poyaowu <r3a2t10@gmail.com>
2025-12-06 09:35:08 +0800

    refine table-cell

--- js/custom.js
+++ js/custom.js
@@ -62,8 +62,6 @@ $(document).ready(function () {
         }
     });
 
-
-
     // Parallax
     var parallax = function () {
         $(window).stellar();
@@ -231,11 +229,7 @@ $(document).ready(function () {
 });
 
 const bgImages = [
-    'img/home-bg-1.jpg',
-    'img/home-bg-2.jpg',
-    'img/home-bg-3.jpg',
-    'img/home-bg-4.jpg',
-    'img/home-bg-5.jpg'
+    'img/home-bg-1.jpg'
 ];
 
 let index = 0;
